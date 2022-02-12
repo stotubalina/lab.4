e0171a9a41571db260e5005aea1adc3d4a2454fa
stotubalina <totubalina02@bk.ru>
2022-02-12 13:41:37 +0300

    /

--- prog/individual_4.py
+++
@@ -1,9 +0,0 @@
-import math
-
-R = float(input("Введите значение внешнего радиуса окружности: "))
-r = float(input("Введите значение внутреннего радиуса окружности: "))
-SR = math.pi * (R**2)
-Sr = math.pi * (r**2)
-S = SR - Sr
-S = float('{:.3f}'.format(S))
-print('Площадь кольца = ', S)
